22b89d7420a5041782f8c0f4191bab6142b93482
KhushiSingh05 <ksingh16851@gmail.com>
2024-06-01 18:19:47 +0530

    khushi

--- src/App.js
+++ src/App.js
@@ -2,6 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //let str = "hello"
+  //let str2 = "world"
+  //if (str === "hello" && str2 === "world")
+  //{
+  //  console.log("hello world")
+ // } 
+ const student1 ={
+  name:"Ansh",
+  address:"Patna",
+  roll:"05"
+ };
+ const student2 ={
+  name:"Ansh",
+  address:"Ranchi",
+  Age:"05"
+ };
+ const fun1 =()=>{
+  const num1=5;
+  const num2=20;
+  const sum = num1+num2;
+  console.log(sum);
+ };
+ if(student1.roll === student2.Age){
+  fun1();
+ }
+ else{
+  console.log("hello");
+ }
+ 
   return (
     <div className="App">
       <header className="App-header">
