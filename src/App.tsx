e2d8498a685cf09544faa914984300a655e8e3fc
KhushiSingh05 <ksingh16851@gmail.com>
2024-06-11 18:27:44 +0530

    khushi2

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<number>(0);
+  const [numberData, setNumberData] = useState<number>(0);
+
+  const numberArray = [1,2,3,4,5,6,7,8];
+  const people =[
+    {name : "Dishh", Age : 150,status:"active"},
+    {name : "Puja", Age :250,status:"inactive"},
+    {name : "Sam", Age : 80,status:"active"}
+  ];
+  useEffect(() => {
+    addData();
+  }, [numberData]);
+    
+  const addData = () => {
+    const oldData = data;
+    const newData = oldData+1;
+    setData(newData);
+    console.log(newData);
+  };
+  const decrementData = () =>{
+    setNumberData (numberData - 1);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <button title = "title" onClick={addData} className={`number ${data}`}>
+      add Data
+    </button>
+    <div>Incremented Data: {data}</div>
+      <br/>
+      <button onClick={decrementData} className={`decrement ${numberData}`}>
+        decrement
+      </button>
+      <div>decremented Data: {numberData}</div>
+      {numberArray.map((item ,i ) => {
+        return <div key= {i}>{item}</div>
+       })}
+       {/*map with object array*/}
+       {people.map((item,i) => {
+        return(
+          <div key={i}>
+            {item.status === "active" &&(
+             <>
+             <div>name: {item.name}</div>
+             <div>Age: {item.Age}</div>
+             <br />
+             </>
+            )}
+            </div>
+        );
+      })}
+      <hr />
+
+
+       {people.map((item,i) => {
+        return(
+          <div key={i}>
+            {item.status === "active" ?(
+            <>
+            <div>name: {item.name}</div>
+            <div>Age: {item.Age}</div>
+            <br />
+            </>
+            ) : (
+            <>
+            <div>inactive</div>
+            <br />
+            </>
+            )}
+          </div>
+        );
+       })}
     </>
-  )
+  );
 }
 
 export default App
