c05083637ab383bf2388cab6e1902088868d8ba9
KhushiSingh05 <ksingh16851@gmail.com>
2024-06-18 18:26:20 +0530

    khushi8

---
+++ src/App.js
@@ -0,0 +1,105 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  //let str = "hello"
+  //let str2 = "world"
+  //if (str === "hello" && str2 === "world")
+  //{
+  //  console.log("hello world")
+ // } 
+ /*const student1 ={
+  name:"Ansh",
+  address:"Patna",
+  roll:"05"
+ };
+ const student2 ={
+  name:"Ansh",
+  address:"Ranchi",
+  Age:"05"
+ };
+ const fun1 =()=>{
+  const num1=5;
+  const num2=20;
+  const sum = num1+num2;
+  console.log(sum);
+ };
+ if(student1.roll === student2.Age){
+  fun1();
+ }
+ else{
+  console.log("hello");
+ }
+ const arr1 =[1,2,3,4,5,6];
+ for(let item of arr1){
+  console.log(item);
+ }*/
+ //const people =[
+ // {name : "Ansh", age :19},
+  //{name : "Disha", age :18},
+  //{name : "Sweety", age :20} 
+ //];
+ //for( let person of people ){
+  //console.log(`Name: ${person.name}, Age: ${person.age}`);
+// }
+//people.forEach((person) => {
+//  console.log(`Name: ${person.name}, Age: ${person.age}`);
+//});
+/*const employees = [
+  {name : "Raj", Age: 22, Salary : 800},
+  {name: "Ansh", Age: 50, Salary : 900},
+  {name: "Khushi", Age: 20, Salary : 1200},
+  {name: "Disha", Age: 55, Salary : 1400},
+];
+const mappedEmployees = employees.map((item) => {
+  return {
+    ...item,
+    Salary: item.Salary+100,
+  };
+},[]);*/
+//console.log("Employees", employees);
+//console.log("mappedEmployees", mappedEmployees);
+
+//console.log(employee.name);
+//for(const key in employee){
+ // if(employee.hasOwnProperty(key)) {
+ // console.log(`${key}: ${employee[key]}`);
+//}
+//}
+/*const products =[
+  {id: 1001 , name: "Laptop", price:1500, category: "Electronics"},
+  {id: 1002 , name: "Shirt", price:800, category: "clothing"},
+  {id: 1003 , name: "Coffee Maker", price:100, category: "Home Appliance"},
+  {id:1004 , name: "Shirts", price:600, category: "Clothing"},
+];
+const filteredProducts = products.filter((item) => {
+  return item.category === "clothing";
+});
+console.log("filteredProducts", filteredProducts);
+const foundProducts = products.find((item) => {
+  return item.id === 1002;
+});*/
+/***************************************to create a new branch******************************* */
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/pages/createStudent.tsx
+++ src/pages/createStudent.tsx
@@ -1,21 +1,33 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { IStudent } from '../interface/student';
+import React from "react";
+import { IStudent } from "../interface/student";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
-const CreateStudent : React.FC  = ():any => {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<IStudent>({mode: "all"});
+const CreateStudent: React.FC = () => {
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IStudent>({ mode: "all" });
 
-    const createNewStudent = (data: IStudent) =>{
-        console.log(data);
-    }
+  const createNewStudent = (data: IStudent) => {
+    console.log(data);
+    
+  localStorage.setItem("students",JSON .stringify(data));
+    // const students: IStudent[] = JSON.parse(
+    //   localStorage.getItem("students") as string
+    // );
+    // data._id = (students.length + 1).toString();
 
+    // students.push(data);
+    // localStorage.setItem("students", JSON.stringify(students));
+    // navigate("/student/student-details");
+  };
   return (
     <>
-       <div className="create_student_section">
+      <div className="create_student_section">
+        Lorem, ipsum.
         <form onSubmit={handleSubmit(createNewStudent)}>
           <div>
             <label htmlFor="name"></label>
@@ -37,6 +49,7 @@ const CreateStudent : React.FC  = ():any => {
               id="age"
               {...register("age", {
                 required: { value: true, message: "Age is required" },
+                valueAsNumber: true,
               })}
             />
             <div className="error_message">{errors.age?.message}</div>
@@ -49,6 +62,7 @@ const CreateStudent : React.FC  = ():any => {
               id="phone"
               {...register("phone", {
                 required: { value: true, message: "Phone is required" },
+                valueAsNumber: true,
               })}
             />
             <div className="error_message">{errors.phone?.message}</div>
@@ -71,8 +85,8 @@ const CreateStudent : React.FC  = ():any => {
           </button>
         </form>
       </div>
-      </>
-  );    
+    </>
+  );
 };
 
 export default CreateStudent;
