931d3390368b8441f99a540846cf77ee3d88b64b
KhushiSingh05 <ksingh16851@gmail.com>
2024-06-20 18:24:31 +0530

    khushidishh

--- src/pages/Students.tsx
+++ src/pages/Students.tsx
@@ -2,24 +2,25 @@ import React, { useEffect, useState } from "react";
 import{ IStudent } from "../interface/student";
 import { Link } from "react-router-dom";
 
+import "./Students.scss";
 
 const Students: React.FC = () => {
+
   const [allStudents, setAllStudents ] = useState<IStudent[]>();
-  useEffect(() => {
-    const students: IStudent[] | undefined = JSON.parse(
-      localStorage.getItem("students") as string 
-    );
-    console.log(students);
-    setAllStudents(students);
-  }, []);
+  useEffect(()=>{
+    const student = JSON.parse(localStorage.getItem("students")as string);
+    setAllStudents(student)
+console.log("student data",allStudents);
+  },[]);
+
+
+ 
   return( 
   <> 
-  <button title="Create Student">
+  <button title="Create Student" className="create_button">
     <Link to="/student/create-student">Create Student</Link>
   </button>
-  <br />
-      <hr />
-      <table>
+  <table>
         <thead>
           <tr>
             <td>SN.</td>
@@ -43,7 +44,10 @@ const Students: React.FC = () => {
           })}
         </tbody>
       </table>
-    </>
+  <br />
+  <hr />
+     
+  </>
   );
 };
 
